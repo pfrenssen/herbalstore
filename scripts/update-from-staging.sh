2046323e2bfca78b37153fb14770333372b1a3da
Pieter Frenssen <pieter@frenssen.be>
2020-06-16 10:18:50 +0300

    Update documentation.

--- scripts/update-from-staging.sh
+++ scripts/update-from-staging.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
+# Get the latest code from the github repository.
 git pull
+
+# Install any new or updated dependencies.
 ./vendor/bin/composer install
+
+# Regenerate the configuration files since they might have changed.
 ./vendor/bin/robo dev:setup
+
+# Copy the database.
 ./vendor/bin/drush sql:sync @staging @self --yes
+
+# Copy the files and images.
 ./vendor/bin/drush rsync @staging:%files @self:%files --yes
+
+# Clear the cache.
 ./vendor/bin/drush cr
