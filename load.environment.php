6fbff809314a00e552a1883ad16af5e7cef28fa6
Pieter Frenssen <pieter@frenssen.be>
2020-06-17 22:44:46 +0300

    Tests are failing on Travis CI (#64)

--- load.environment.php
+++ load.environment.php
@@ -8,9 +8,20 @@
 use Dotenv\Dotenv;
 
 // Load environment variables.
-$dotenv = Dotenv::createImmutable(__DIR__, ['.env.dist', '.env'], FALSE);
-$dotenv->load();
+foreach (['.env', '.env.dist'] as $file) {
+  $path = __DIR__ . DIRECTORY_SEPARATOR . $file;
+  if (is_file($path) && is_readable($path)) {
+    $dotenv = Dotenv::createImmutable(__DIR__, $file);
+    $dotenv->load();
+  }
+}
 
 // Set dynamic environment variables.
-$_ENV['PROJECTROOT'] = __DIR__;
-$_ENV['WEBROOT'] = __DIR__ . '/web';
+$dynamic_vars = [
+  'PROJECTROOT' => __DIR__,
+  'WEBROOT' => __DIR__ . DIRECTORY_SEPARATOR . 'web',
+];
+foreach ($dynamic_vars as $name => $value) {
+  $_ENV[$name] = $value;
+  $_SERVER[$name] = $value;
+}
