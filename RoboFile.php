72d137b0f888099a965f50b40ac9765b12d8df2e
Pieter Frenssen <pieter@frenssen.be>
2020-06-27 16:49:20 +0300

    Set up basic authentication.

--- RoboFile.php
+++ RoboFile.php
@@ -2,12 +2,16 @@
 
 declare(strict_types = 1);
 
+use Psr\Log\LoggerAwareInterface;
+use Psr\Log\LoggerAwareTrait;
 use Robo\Tasks;
 
 /**
  * Robo task runner configuration for the Herbal Store project.
  */
-class RoboFile extends Tasks {
+class RoboFile extends Tasks implements LoggerAwareInterface {
+
+  use LoggerAwareTrait;
 
   /**
    * Sets up the development environment.
@@ -45,6 +49,61 @@ class RoboFile extends Tasks {
     $this->output()->writeln('$ cp ./web/sites/default/default.services.yml ./web/sites/default/services.yml');
     $this->output()->writeln('Uncomment the section about settings.local.php in ./web/sites/default/settings.php');
     $this->output()->writeln('Set `debug = true` in ./web/sites/default/services.yml');
+  }
+
+  /**
+   * Enables or disables HTTP Basic Authentication.
+   *
+   * This command will enable HTTP Basic Authentication if the credentials are
+   * set in the environment variables BASIC_AUTH_USERNAME and BASIC_AUTH_PASSWORD.
+   *
+   * If either of these variables are not set basic authentication will be disabled.
+   *
+   * Make sure to run this every time after scaffolding Drupal core.
+   */
+  public function environmentSetupBasicAuth(): void {
+    // Remove the existing .htpasswd file.
+    $this->taskFilesystemStack()->remove('web/.htpasswd')->run();
+
+    // Remove the basic auth directive from the .htaccess file if it exists.
+    $this->taskReplaceInFile('web/.htaccess')
+      //->regex('/^AuthType Basic.*Require valid-user$/m')
+      ->regex('/^AuthType Basic\n.*\n.*\nRequire valid-user\n/m')
+      ->to('')
+      ->run();
+
+    // Check that the basic authentication credentials are defined.
+    foreach (['BASIC_AUTH_USERNAME', 'BASIC_AUTH_PASSWORD'] as $env_var) {
+      if (empty($_SERVER[$env_var])) {
+        $this->logger->notice('Basic authentication has been disabled since the credentials are not set.');
+        return;
+      }
+    }
+
+    // Generate a fresh .htpasswd file.
+    $result = $this->taskExec('htpasswd')
+      ->option('-b') // Provide the password as an argument.
+      ->option('-c') // Create a new file.
+      ->arg('web/.htpasswd')
+      ->arg($_SERVER['BASIC_AUTH_USERNAME'])
+      ->arg($_SERVER['BASIC_AUTH_PASSWORD'])
+      ->run();
+
+    if (!$result->wasSuccessful()) {
+      $this->logger->error('.htpasswd file could not be written');
+    }
+
+    // Add the directive to enable basic authentication.
+    $htpasswd_path = getcwd() . '/web/.htpasswd';
+    $this->taskWriteToFile('web/.htaccess')
+      ->append()
+      ->lines([
+        'AuthType Basic',
+        'AuthName "Restricted access"',
+        'AuthUserFile ' . $htpasswd_path,
+        'Require valid-user',
+      ])
+      ->run();
   }
 
   /**
