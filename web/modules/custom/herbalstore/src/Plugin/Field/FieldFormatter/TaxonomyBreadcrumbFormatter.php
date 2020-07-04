e996d5d65debbc7756d4f21d30d4c87303ba4027
Pieter Frenssen <pieter@frenssen.be>
2020-07-04 23:50:23 +0300

    Render the product category as breadcrumbs.

---
+++ web/modules/custom/herbalstore/src/Plugin/Field/FieldFormatter/TaxonomyBreadcrumbFormatter.php
@@ -0,0 +1,114 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Drupal\herbalstore\Plugin\Field\FieldFormatter;
+
+use Drupal\Core\Datetime\DateFormatterInterface;
+use Drupal\Core\Entity\EntityStorageInterface;
+use Drupal\Core\Entity\EntityTypeManager;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Field\FieldDefinitionInterface;
+use Drupal\Core\Field\FieldItemListInterface;
+use Drupal\Core\Field\FormatterBase;
+use Drupal\Core\Field\Plugin\Field\FieldType\EntityReferenceItem;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\taxonomy\TermInterface;
+use Drupal\taxonomy\TermStorageInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Plugin implementation of the 'Taxonomy breadcrumb' field formatter.
+ *
+ * @FieldFormatter(
+ *   id = "herbalstore_taxonomy_breadcrumb",
+ *   label = @Translation("Taxonomy breadcrumb"),
+ *   field_types = {
+ *     "entity_reference",
+ *   }
+ * )
+ */
+class TaxonomyBreadcrumbFormatter extends FormatterBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * The entity type manager.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * Constructs a new TaxonomyBreadcrumbFormatter.
+   *
+   * @param string $plugin_id
+   *   The plugin_id for the formatter.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\Core\Field\FieldDefinitionInterface $field_definition
+   *   The definition of the field to which the formatter is associated.
+   * @param array $settings
+   *   The formatter settings.
+   * @param string $label
+   *   The formatter label display setting.
+   * @param string $view_mode
+   *   The view mode.
+   * @param array $third_party_settings
+   *   Third party settings.
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+   *   The entity type manager.
+   */
+  public function __construct($plugin_id, $plugin_definition, FieldDefinitionInterface $field_definition, array $settings, $label, $view_mode, array $third_party_settings, EntityTypeManagerInterface $entity_type_manager) {
+    parent::__construct($plugin_id, $plugin_definition, $field_definition, $settings, $label, $view_mode, $third_party_settings);
+
+    $this->entityTypeManager = $entity_type_manager;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $plugin_id,
+      $plugin_definition,
+      $configuration['field_definition'],
+      $configuration['settings'],
+      $configuration['label'],
+      $configuration['view_mode'],
+      $configuration['third_party_settings'],
+      $container->get('entity_type.manager')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function viewElements(FieldItemListInterface $items, $langcode) {
+    $element = [
+      '#theme' => 'item_list__taxonomy_breadcrumb',
+      '#list_type' => 'ol',
+      '#items' => [],
+      '#attributes' => ['class' => 'breadcrumb'],
+      '#wrapper_attributes' => ['class' => 'category-subcategory'],
+    ];
+
+    /** @var \Drupal\Core\Field\Plugin\Field\FieldType\EntityReferenceItem $item */
+    $item = $items->first();
+    /** @var \Drupal\taxonomy\TermInterface $term */
+    $term = $item->entity;
+    /** @var \Drupal\taxonomy\TermStorageInterface $term_storage */
+    $term_storage = $this->entityTypeManager->getStorage('taxonomy_term');
+    $parents = $term_storage->loadAllParents($term->id());
+    foreach ($parents as $parent) {
+      $link = [
+        '#wrapper_attributes' => ['class' => ['breadcrumb-item']],
+        '#title' => $parent->label(),
+        '#type' => 'link',
+        '#url' => $parent->toUrl(),
+      ];
+      array_unshift($element['#items'], $link);;
+    }
+
+    return $element;
+  }
+
+}
