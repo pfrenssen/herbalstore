d9b1e399571a3e0bcaf5a0a49fffe9683d6555ef
Pieter Frenssen <pieter@frenssen.be>
2020-08-27 01:44:10 +0300

    Set the temporary folder inside the build so it can be used in the Lando environment.

--- resources/lando/settings.local.php
+++ resources/lando/settings.local.php
@@ -13,3 +13,4 @@ $databases['default']['default'] = [
 $settings['hash_salt'] = md5(getenv('LANDO_HOST_IP'));
 $settings['trusted_host_patterns'][] = '^.*\.lndo\.site$';
 $settings['config_sync_directory'] = '../config/sync';
+$settings['file_temp_path'] = '../tmp';
