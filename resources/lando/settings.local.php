507cc2cc264ec09d8a22755f4dfa87e3a064ed3b
Pieter Frenssen <pieter@frenssen.be>
2020-08-27 00:34:51 +0300

    Add settings overrides for the Lando environment.

---
+++ resources/lando/settings.local.php
@@ -0,0 +1,15 @@
+<?php
+
+$databases['default']['default'] = [
+  'driver' => 'mysql',
+  'database' => 'drupal8',
+  'username' => 'drupal8',
+  'password' => 'drupal8',
+  'host' => 'database',
+  'port' => 3306,
+  'prefix' => '',
+];
+
+$settings['hash_salt'] = md5(getenv('LANDO_HOST_IP'));
+$settings['trusted_host_patterns'][] = '^.*\.lndo\.site$';
+$settings['config_sync_directory'] = '../config/sync';
