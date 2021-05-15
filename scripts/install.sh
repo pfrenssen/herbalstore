193dce00867d3f2ec026893982a3d8b24c729390
Pieter Frenssen <pieter@frenssen.be>
2021-05-15 20:56:00 +0300

    Rework scripts to retrieve data from the latest backup rather than from production.

---
+++ scripts/install.sh
@@ -0,0 +1,32 @@
+#!/bin/bash -e
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+cd $SCRIPT_DIR/..
+
+# Create environment specific files.
+LANDO_PROJECT_NAME=herbalstore envsubst < resources/lando/.lando.yml > .lando.yml
+LANDO_PROJECT_NAME=herbalstore envsubst < resources/lando/.env > .env
+
+# Start Lando.
+lando start
+
+# Generate the configuration files.
+lando robo dev:setup
+
+# Sync the database and files from the latest backup.
+mkdir -p userfiles
+rsync -az --delete desktop.office.lan:/mnt/workspace/sites/zonnedauw/backups/daily.0/ userfiles
+
+# Restore the database.
+zcat userfiles/dump.sql.gz | lando drush sql:cli
+
+# Hardlink the files folder. Remove the existing destination since an empty
+# folder might be scaffolded here.
+rm -rf web/sites/default/files
+cp -al userfiles/files web/sites/default/
+
+# Clear the cache.
+lando drush cr
+
+# Index content in the search engine.
+lando drush sapi-i
