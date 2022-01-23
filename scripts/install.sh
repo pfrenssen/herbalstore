263b14de96433eb44ce1a48f09c826f89d566f0b
Pieter Frenssen <pieter@frenssen.be>
2022-01-23 19:48:37 +0200

    Backport improvements to install and update scripts from another project.

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,4 +1,6 @@
-#!/bin/bash -e
+#!/bin/bash -ex
+# Installs a development environment which mimics production.
+# For best results, check out the master branch before executing this script.
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 cd $SCRIPT_DIR/..
@@ -7,25 +9,34 @@ cd $SCRIPT_DIR/..
 LANDO_PROJECT_NAME=herbalstore envsubst < resources/lando/.lando.yml > .lando.yml
 LANDO_PROJECT_NAME=herbalstore envsubst < resources/lando/.env > .env
 
-# Start Lando.
-lando start
+# Rebuild Lando.
+lando rebuild -y
 
-# Ensure folders are writable.
-chmod u+w -R web/sites/default/
+# Install dependencies.
+lando composer install
 
 # Generate the configuration files.
 lando robo dev:setup
 
 # Restore the database.
+lando drush sql-drop -y
 zcat userfiles/dump.sql.gz | lando drush sql:cli
 
 # Hardlink the files folder. Remove the existing destination since an empty
 # folder might be scaffolded here.
+chmod u+w -R web/sites/default 2> /dev/null
 rm -rf web/sites/default/files
 cp -al userfiles/files web/sites/default/
+
+# Create temporary files folder and private files folder.
+mkdir -p tmp/
+mkdir -p private/
 
 # Clear the cache.
 lando drush cr
 
 # Index content in the search engine.
 lando drush sapi-i
+
+# Show project info.
+lando info
--- scripts/update.sh
+++ scripts/update.sh
@@ -5,6 +5,13 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 cd $SCRIPT_DIR/..
 
+# Create environment specific files.
+LANDO_PROJECT_NAME=herbalstore envsubst < resources/lando/.lando.yml > .lando.yml
+LANDO_PROJECT_NAME=herbalstore envsubst < resources/lando/.env > .env
+
+# Rebuild Lando.
+lando rebuild -y
+
 # Install any new or updated dependencies.
 lando composer install
 
@@ -12,12 +19,18 @@ lando composer install
 lando robo dev:setup
 
 # Restore the database.
+lando drush sql-drop -y
 zcat userfiles/dump.sql.gz | lando drush sql:cli
 
 # Hardlink the files folder. Remove the existing destination since an empty
 # folder might be scaffolded here.
+chmod u+w -R web/sites/default 2> /dev/null
 rm -rf web/sites/default/files
 cp -al userfiles/files web/sites/default/
+
+# Create temporary files folder and private files folder.
+mkdir -p tmp/
+mkdir -p private/
 
 # Perform updates.
 lando drush deploy --yes
